c7f3a5fcde18511c671e8b1daced50afd1b468b0
Pomidorenen <matras235@gmail.com>
2022-12-15 20:52:44 +0500

    v0.4

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 var geometry = new THREE.BoxGeometry( 10, 10, 10);
 var group = new THREE.Object3D();
+var group2 = new THREE.Object3D();
+var group3 = new THREE.Object3D();
+var group4 = new THREE.Object3D();
 var light= new THREE.DirectionalLight(0xffffff,1)
 light.position.x=100
 light.position.y=100
@@ -12,19 +15,48 @@ light.position.z=100
 ao=new THREE.AmbientLight(0xffffff,0.5)
 group.receiveShadow = true
 scene.background = new THREE.Color(0xf2af02);
-cubic()
+cubic(group)
+cubic(group2)
+cubic(group3)
+cubic(group4)
+group2.position.x=75
+group3.position.x=-75
+group4.position.x=-150
+plane = new THREE.Mesh(new THREE.BoxGeometry( 300, 300, 300), new THREE.MeshPhongMaterial({ color: 0xfff}));
+plane.position.x=0
+plane.rotation.x = - Math.PI / 2
+plane.receiveShadow = true
+scene.add(plane);
+scene.add( group2 );
 scene.add( group );
+scene.add( group3 );
+scene.add( group4 );
 scene.add(ao)
 scene.add(light)
 camera.position.z = 105;
+i=0
 function render() {
     requestAnimationFrame( render );
+    // i+=20
+    // ai=i/Math.PI
+    // ai=Math.cos(ai)
+    // for(i=0;i<27;i++)
+    // {
+    //         group.children[i].rotation.z+=ai
+
+    // }
+    group2.rotation.x-=0.001
+    group2.rotation.y-=0.003
     group.rotation.x+=0.001
     group.rotation.y+=0.003
+    group3.rotation.z+=0.001
+    group3.rotation.y+=0.003
+    group4.rotation.x-=0.001
+    group4.rotation.z-=0.003
   renderer.render( scene, camera );
 }
 render();
-function cubic()
+function cubic(gr)
 {
     for(j=0;j<3;j++)
 {
@@ -42,7 +74,7 @@ function cubic()
         cube.position.z=j*10;
         cube.position.x=10*c;
         cube.position.y=10*r;
-        group.add(cube)
+        gr.add(cube)
     }
 }
 
